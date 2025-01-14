13a5d39a1dc9e82a7aa2b277cbc5a3b317c038ba
bhavya shingari <bhavyashingari114@gmail.com>
2025-01-14 12:28:05 +0530

    Final changes before deployment

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -2,8 +2,8 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const msalConfig = {
   auth: {
-    clientId: "YOUR_CLIENT_ID",  
-    authority: "https://login.microsoftonline.com/YOUR_TENANT_ID",  
+    clientId: ProcessingInstruction.event.REACT_APP_AZURE_CLIENT_ID,  
+    authority: "https://login.microsoftonline.com/{REACT_APP_AZURE_TENANT_ID}",  
     redirectUri: "http://localhost:3000",  
   },
   cache: {
